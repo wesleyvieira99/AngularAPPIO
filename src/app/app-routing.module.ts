da47345b386a633c30f8a127971ad5175a71648d
Wesley Vieira <wesleysv11@gmail.com>
2023-03-21 06:39:09 -0300

    primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { HomeComponent } from './navegacao/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path:'home', component: HomeComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/navegacao/navegacao.module.ts
@@ -0,0 +1,28 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent
+  ]
+})
+export class NavegacaoModule { }
